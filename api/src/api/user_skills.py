22fb9d62400a121d372a326f60219626c1fed263
Albert Suarez <alsumo95@gmail.com>
2019-03-16 23:02:34 +0000

    Tweaked /user/skills endpoints

--- api/src/api/user_skills.py
+++ api/src/api/user_skills.py
@@ -1,4 +1,7 @@
 from flask import jsonify, request
+
+from src.model.tag import Tag
+from src.model.user import User
 from src.util import log
 from src.db.sqlalchemy import db_session
 from src.model.user_to_tag import UserToTag
@@ -13,31 +16,59 @@ def get(user_id):
         return jsonify(error=True, message='Unexpected error.'), 400
 
 
-def post(user_id, tag_id):
+def post():
     try:
-        usuari_tag = UserToTag(
-            user_id=user_id,
-            tag_id=tag_id
-        )
-        db_session().add(usuari_tag)
-        db_session().flush()
-        db_session().commit()
-        return jsonify(error=False, response=''), 200
+        body = request.json
+        required_parameters = ['user_id', 'tag_id']
+        if not all(x in body for x in required_parameters):
+            return jsonify(error=True, message='All request body parameters are required.'), 400
+
+        user = db_session().query(User).filter_by(id=body['user_id']).first()
+        if not user:
+            return jsonify(error=True, message='No user found with {} as id.'.format(body['user_id'])), 400
+
+        tag = db_session().query(Tag).filter_by(id=body['tag_id']).first()
+        if not tag:
+            return jsonify(error=True, message='No tag found with {} as id.'.format(body['tag_id'])), 400
+
+        user_tag = db_session().query(UserToTag).filter_by(user_id=body['user_id'], tag_id=body['tag_id']).first()
+        if not user_tag:
+            user_tag = UserToTag(
+                user_id=body['user_id'],
+                tag_id=body['tag_id']
+            )
+            db_session().add(user_tag)
+            db_session().flush()
+            db_session().commit()
+        return jsonify(error=False, response=user_tag.serialize()), 200
     except Exception as e:
         log.error('Unexpected error in POST/user/skills: {}'.format(e))
         return jsonify(error=True, message='Unexpected error.'), 400
 
 
-def delete(user_id, tag_id):
+def delete():
     try:
-        usuari_tag = db_session().query(UserToTag).filter_by(user_id=user_id, tag_id=tag_id).first()
-        if usuari_tag:
-            db_session().delete(usuari_tag)
+        body = request.json
+        required_parameters = ['user_id', 'tag_id']
+        if not all(x in body for x in required_parameters):
+            return jsonify(error=True, message='All request body parameters are required.'), 400
+
+        user = db_session().query(User).filter_by(id=body['user_id']).first()
+        if not user:
+            return jsonify(error=True, message='No user found with {} as id.'.format(body['user_id'])), 400
+
+        tag = db_session().query(Tag).filter_by(id=body['tag_id']).first()
+        if not tag:
+            return jsonify(error=True, message='No tag found with {} as id.'.format(body['tag_id'])), 400
+
+        user_tag = db_session().query(UserToTag).filter_by(user_id=body['user_id'], tag_id=body['tag_id']).first()
+        if user_tag:
+            db_session().delete(user_tag)
             db_session().commit()
-            return jsonify(error=False, response=''), 200
+            return jsonify(error=False, response='Deleted'), 200
         else:
             return jsonify(error=True, message='No skill found for {} as user id, and {} as skill_id'
-                           .format(user_id, tag_id)), 400
+                           .format(body['user_id'], body['tag_id'])), 400
     except Exception as e:
         log.error('Unexpected error in POST/user/skills: {}'.format(e))
         return jsonify(error=True, message='Unexpected error.'), 400
