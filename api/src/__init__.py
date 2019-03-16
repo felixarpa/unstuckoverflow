65f1fceda2d196176d2e874876890d81a7e6cba2
Albert Suarez <alsumo95@gmail.com>
2019-03-16 22:31:34 +0000

    Implemented user/POST

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -4,10 +4,18 @@ DB_PORT = 5432
 
 TAG_CSV_FILE = 'src/db/QueryResults.csv'
 
+NO_COMPANY_EMAILS = [
+    'gmail',
+    'hotmail',
+    'yahoo',
+    'outlook'
+]
+
 
 __all__ = [
     'DB_USER',
     'DB_DB',
     'DB_PORT',
-    'TAG_CSV_FILE'
+    'TAG_CSV_FILE',
+    'NO_COMPANY_EMAILS'
 ]
