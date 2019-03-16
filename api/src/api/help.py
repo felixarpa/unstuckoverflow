20a5d22bd8d202100a2cd49ce3fc73bf22d98dc9
Albert Suarez <alsumo95@gmail.com>
2019-03-16 20:48:51 +0000

    Started /help implementation

--- api/src/api/help.py
+++ api/src/api/help.py
@@ -1,9 +1,21 @@
 from flask import jsonify, request
+
+from src.model.user import User
 from src.util import log
+from src.db.sqlalchemy import db_session
 
 
 def post():
     try:
+        body = request.json
+        required_parameters = ['page_html', 'user_id']
+        if not all(x in body for x in required_parameters):
+            return jsonify(error=True, message='All request body parameters are required.'), 400
+
+        user = db_session().query(User).filter_by(id=body['user_id']).first()
+        if not user:
+            return jsonify(error=True, message='No user found with {} as id.'.format(body['user_id'])), 400
+
         return 'You made it'
     except Exception as e:
         log.error('Unexpected error in POST/help: {}'.format(e))
