0f6c98b94acb5c17d8f82a0d982bf01ee79b4074
Albert Suarez <alsumo95@gmail.com>
2019-03-17 13:48:53 +0000

    Pointed to localhost

--- client/public/contentScripts.js
+++ client/public/contentScripts.js
@@ -1,38 +1,30 @@
 /* global chrome */
-console.log(chrome.storage);
-chrome.storage.local.get(['userId'], (res) => {
-  console.log(res);
-  let userId = res.userId;
+const userId = 2;
 
-  if (userId == null) {
-    userId = 2;
-  }
-
-  var htmlCode = document.documentElement.outerHTML;
-  var data = JSON.stringify({
-    "page_html": htmlCode,
-    "user_id": userId
-  });
-
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  xhr.onload = function(e) {
-    if (this.status === 200) {
-      if (!this.response.error) {
-        if (this.response.response.skills.length > 0) {
-          var alertMessage = this.response.response.user.full_name + ' knows about [' +
-                            this.response.response.skills.join(', ') + ']. You should ask them about your doubt!';
-          alert(alertMessage)
-        }
-      } else {
-        console.log('Error in the request:', this.response)
-      }
-    }
-  };
-
-  xhr.open("POST", "http://35.187.102.14:8081/help");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.setRequestHeader("cache-control", "no-cache");
-
-  xhr.send(data);
+var htmlCode = document.documentElement.outerHTML;
+var data = JSON.stringify({
+  "page_html": htmlCode,
+  "user_id": userId
 });
+
+var xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
+xhr.onload = function(e) {
+  if (this.status === 200) {
+    if (!this.response.error) {
+      if (this.response.response.skills.length > 0) {
+        var alertMessage = this.response.response.user.full_name + ' knows about [' +
+                          this.response.response.skills.join(', ') + ']. You should ask them about your doubt!';
+        alert(alertMessage)
+      }
+    } else {
+      console.log('Error in the request:', this.response)
+    }
+  }
+};
+
+xhr.open("POST", "http://localhost:8081/help");
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("cache-control", "no-cache");
+
+xhr.send(data);
